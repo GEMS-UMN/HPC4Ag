d71cca607f9ed9c7e00e9434d648c0b81d8d4441
Jesse Erdmann <jerdmann@umn.edu>
2025-03-11 16:21:46 -0500

    Clean up jupyter start in slurmctld

--- start_slurmctld.sh
+++ start_slurmctld.sh
@@ -3,7 +3,5 @@ cat /opt/hpc4ag/etc/hosts >> /etc/hosts
 sudo -u munge munged
 slurmctld
 slurmd
-sudo -u jupyter /bin/bash
-source /opt/hpc4ag/venv/bin/activate
 cd /opt/hpc4ag/github_repo
-jupyter lab --ip 0.0.0.0
+sudo -u jupyter /opt/hpc4ag/venv/bin/jupyter lab --ip 0.0.0.0 --ServerApp.root_dir /opt/hpc4ag/github_repo/ 
